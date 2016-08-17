eb22f906410d347c1043df61c21c3b376395f7a5
John Murray <me@johnmurray.io>
2016-08-17 02:31:10 +0000

    added version, modified package
    The script is now packaged as a real library (only holds the version) and
    the script (still where all the good stuff is at). But this now means that
    I can start refactoring to pull stuff out. So good for me (pats self on
    back). Also,  now does something and the version is set in one place

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from jsoninspectlib import __version__
 
 setup(
     name = 'json-inspect',
-    version = '0.1.4',
+    version = __version__,
 
     author = 'John Murray',
     author_email = 'me@johnmurray.io',
     description = 'JSON inspection command line client',
     license='Apache 2.0',
-    package_dir = {'json_inspect': ''},
-    packages = ['json_inspect'],
+    packages = ['jsoninspectlib'],
     scripts = ['json-inspect'],
     url = 'http://github.com/JohnMurray/json-inspect',
  )
